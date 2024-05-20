35cb3ac0d2ca69eb4206162c425b989283335b50
husein1402 <husein1402@mail.ru>
2024-05-21 00:45:32 +0300

    first commit

---
+++ program3.bat
@@ -0,0 +1,22 @@
+echo off
+md Tursunov
+cd Tursunov
+md Husein
+md Tursunov
+pause
+cd Husein
+echo > 15.txt
+cd..
+cd Tursunov
+echo > 14022003.txt
+cd..
+cd..
+pause
+del Tursunov /S /Q /F
+pause
+cd Tursunov
+rd Tursunov
+rd Husein
+cd..
+rd Tursunov
+pause
